b12c3a863b35ec9e2707e198a83e1b8ef53ea990
Dirk Schuermans <d.schuermans@scia.net>
2019-01-15 13:24:02 +0100

    Added files

---
+++ UpdateNugetDependencyVersions/UpdateNugetDependencyVersions.ps1
@@ -0,0 +1,112 @@
+﻿[CmdletBinding()]
+param(
+    [string]$PackagesFolder,
+    [string]$UseNupk
+    [string]$UseVerbose = "false"
+)
+begin
+{
+    $CurrentVerbosePreference = $VerbosePreference;
+
+    if($VerbosePreference -eq "SilentlyContinue" -And $UseVerbose -eq "true") {
+        $VerbosePreference = "continue"
+    }
+
+    Write-Host "Initializing..."
+    Write-Verbose "Packages are stored in: $PackagesFolder"
+
+    if(-Not (Test-Path $PackagesFolder)) {
+        Write-Warning "$PackagesFolder does not exist."
+        exit 0;
+    }
+
+    # Import functions module that we use
+    Import-Module -Name ./modules/UpdateNugetDependencyVersions_Functions -Force
+}
+process
+{
+    Write-Host "Commencing..."
+
+    # Get list of nupkg files
+    $nugetPackages = Get-ChildItem -Path $PackagesFolder -Filter *.nupkg -File
+
+    Write-Verbose "Found $($nugetPackages.Length) NuGet packages in $PackagesFolder"
+
+    $packageDictionary = @{}
+
+    Write-Host "Extracting NuGet packages"
+
+    foreach($nugetPackage in $nugetPackages)
+    {
+        $nugetFileName = $nugetPackage.BaseName;
+
+        Write-Verbose "Processing $nugetFileName"
+        
+        $success = Get-NupkgContent -Nupkg $nugetFileName -Folder $PackagesFolder -Verbose
+
+        if(-Not $success)
+        {
+            Write-Host "Failed to process $nugetFileName"
+            continue
+        }
+
+        $info = Get-NupkgInformation -Nupkg $nugetFileName -Folder "$PackagesFolder\$NugetFileName"
+
+        $packageDictionary[$info.id] = $info
+    }
+
+    Write-host "Checking dependencies"
+   
+    foreach($packageId in $packageDictionary.Keys)
+    {
+        Write-Verbose "Checking dependencies in '$packageId'"
+
+        $package = $packageDictionary[$packageId]
+
+        foreach($dependantPackageId in $package.Dependencies.Keys)
+        {
+            Write-Verbose "Checking dependency on '$dependantPackageId'"
+
+            if($packageDictionary.ContainsKey($dependantPackageId))
+            {
+                $dependantPackage = $packageDictionary[$dependantPackageId];
+
+                $dependencyOnVersion = $package.Dependencies[$dependantPackageId];
+
+                if($dependantPackage.Version -ne $dependencyOnVersion)
+                {
+                    Write-Warning "Package '$packageId' is dependant on '$($dependantPackage.Id), $dependencyOnVersion' but the package is on version $($dependantPackage.Version)"
+                    
+                    $package.Changed = Update-NuspecDependencyVersion -Nuspec $package.Nuspec -DependencyId $dependantPackage.Id -Version $dependantPackage.Version
+                }
+            }
+        }
+
+        if($package.Changed -eq $True)
+        {
+            Write-Verbose "$packageId has been modified, re-creating nupkg file."
+
+            $packageCreated = Set-NupkgContent -Nupkg $package.Nupkg -Folder $PackagesFolder
+
+            if(-Not $packageCreated)
+            {
+                Write-Warning "Not able to create update nupkg file."
+            }
+        }
+
+        Write-Verbose "Cleaning up files for $($package.Nupkg)"
+
+        $cleanedUp = Clear-NupkgContent -Nupkg $package.Nupkg -Folder $PackagesFolder
+
+        if(-Not $cleanedUp)
+        {
+            Write-Warning "Unable to clean up $($package.Nupkg)"
+        }
+    }
+
+}
+end
+{
+    $VerbosePreference = $CurrentVerbosePreference
+}
+
---
+++ UpdateNugetDependencyVersions/modules/UpdateNugetDependencyVersions_Functions.psm1
@@ -0,0 +1,181 @@
+﻿function ReadXmlContent($filePath)
+{
+    try {
+        [xml]$xmlContent = Get-Content $filePath;
+        return $xmlContent;
+    }
+    catch {
+        Write-Error "Failed to read file : $filePath";
+        Write-Error $Error[0].Exception;
+        exit 1;
+    }
+}
+
+<# 
+ .Synopsis
+  Extract the contents of a NuGet package using nuget.exe
+
+ .Description
+  Extract the contents of a NuGet package using nuget.exe
+
+ .Parameter Nupkg
+  The NuGet package to extract
+
+ .Parameter Folder
+  The folder in which the NuGet package resides
+
+ .Parameter ExtractTo
+  The folder to which the NuGet package should be extracted
+
+ .Example
+   # Extract a nuget package
+   Get-NupkgContent('MyPackage-1.0.0.nupkg', 'C:\packages\', 'C:\ExtractedPackages')
+#>
+function Get-NupkgContent2
+{
+    param([string]$Nupkg, [string]$Folder, [string]$ExtractTo)
+
+    try
+    {
+        if(-Not (Test-path $ExtractTo))
+        {
+            New-Item -ItemType Directory $ExtractTo
+        }
+
+        # Use nuget to extract the package
+        .\nuget.exe install $Nupkg -Source $Folder -OutputDirectory $ExtractTo
+
+        return $true;
+    }
+    catch
+    {
+        throw;
+    }
+}
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Get-NupkgContent
+{
+    [cmdletbinding()]
+     param([string]$Nupkg, [string]$Folder)
+
+     try
+     {
+        if(Test-Path("$Folder\$Nupkg"))
+        {
+            Write-Verbose "$Nupkg is already extracted. Skipping."
+            return $true
+        }
+
+        # Rename file to zip
+        #Get-ChildItem "$Folder\$Nupkg.nupkg" | Rename-Item -NewName {  $_.name  -replace ".nupkg",".zip"  }
+
+        #[System.IO.Compression.ZipFile]::ExtractToDirectory("$Folder\$Nupkg.zip", "$Folder\$Nupkg")
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$Folder\$Nupkg.nupkg", "$Folder\$Nupkg")
+
+        # Rename file to nupkg again
+        #Get-ChildItem "$Folder\$Nupkg.zip" | Rename-Item -NewName { $_.Name -replace ".zip", ".nupkg" }
+
+        return $true
+     }
+     catch
+     {
+        return $false
+     }
+}
+
+function Set-NupkgContent
+{
+    [cmdletbinding()]
+     param([string]$Nupkg, [string]$Folder)
+
+     try
+     {
+        # Remove the old nupkg file
+        Remove-Item "$Folder\$Nupkg.nupkg"
+        
+        # Create new nupkg file
+        [System.IO.Compression.ZipFile]::CreateFromDirectory("$Folder\$Nupkg", "$Folder\$Nupkg.nupkg")
+        
+        
+        return $true
+     }
+     catch
+     {
+        return $false
+     }
+}
+
+function Clear-NupkgContent 
+{
+[cmdletbinding()]
+     param([string]$Nupkg, [string]$Folder)
+
+     try
+     {
+        Remove-Item "$Folder\$Nupkg" -Recurse
+      
+        return $true
+     }
+     catch
+     {
+        return $false
+     }
+}
+
+function Get-NupkgInformation
+{
+    [CmdletBinding()]
+    param([string]$Nupkg, [string]$Folder)
+
+    try
+    {
+        $nuspec = Get-ChildItem -Path "$Folder\$Nupk" -Filter "*.nuspec"
+
+        $nuspecXml = ReadXmlContent($nuspec.FullName)
+
+        $id = $nuspecXml.package.metadata.id
+        $version = $nuspecXml.package.metadata.version
+
+        $dependencies = @{}
+
+        foreach($dependency in $nuspecXml.package.metadata.dependencies.dependency)
+        {
+            #Write-Verbose $dependency.dependency.id
+
+            $depId = $($dependency.id)
+            $depVersion = $($dependency.version)
+
+            $dependencies[$depId] = $depVersion
+        }
+
+        return  @{ 'Id' = $id; 'Version' = $version; 'Nuspec' = $nuspec.FullName; 'Nupkg' = $Nupkg; 'Dependencies' = $dependencies; 'Changed' = $False }
+    }
+    catch
+    {
+        throw
+    }
+}
+
+function Update-NuspecDependencyVersion
+{
+    param([string]$Nuspec, [string]$DependencyId, [string]$Version)
+
+    try
+    {
+        $nuspecXml = ReadXmlContent($Nuspec)
+
+        $dependency = $nuspecXml.package.metadata.dependencies.dependency | where { $_.id -eq $DependencyId }
+        
+        $dependency.version = $Version
+
+        $nuspecXml.Save($Nuspec)
+
+        return $true;
+
+    }
+    catch
+    {
+        return $false;
+    }
+}
